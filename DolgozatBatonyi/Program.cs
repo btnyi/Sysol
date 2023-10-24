b58c5ab2133677e3acb271b96b6fb19cb78e27b3
Bátonyi Dávid <144111995+btnyi@users.noreply.github.com>
2023-10-24 13:35:11 +0200

    Sysol

---
+++ DolgozatBatonyi/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace DolgozatBatonyi
+{
+    class Program
+    {
+
+        static double atlag(List<Solsys> a)
+        {
+
+            return a.Average(x => x.holdSzam);
+        
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            var solsys = new List<Solsys>();
+            using (var sr = new StreamReader(@"..\..\src\solsys.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    solsys.Add(new Solsys(sr.ReadLine()));
+
+                }
+            
+            }
+
+            Console.WriteLine($"3.1: {solsys.Count} bolygó van a naprendszerben");
+            Console.WriteLine($"3.2: A naprendszerben egy bolygónak átlagosan {atlag(solsys)} holdja van");
+
+            var f3 = solsys.OrderBy(x => x.terfogat).Last();
+            Console.WriteLine($"3.3: A legnagyobb térfogatú bolygó: {f3.bolygo}");
+            Console.Write("3.4: Írd be a keresett bolygó nevét: ");
+            string nev = Console.ReadLine();
+            var f4 = solsys.SingleOrDefault(x => x.bolygo == nev);
+            Console.WriteLine(f4 != null ? "Van ilyen bolygó a naprendszerben." : "sajnos nincs ilyen nevű bolygó a naprendszerben");
+            Console.Write("3.5: Írj be egy egész számot: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            var f5 = solsys.Where(x => x.holdSzam > szam);
+            Console.WriteLine($"A következő bolygónak van {szam}-nál/nél több holdja: ");
+            foreach (var item in f5)
+            {
+                Console.Write($"[{item.bolygo}]");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ DolgozatBatonyi/Solsys.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolgozatBatonyi
+{
+    class Solsys
+    {
+        public string bolygo { get; set; }
+        public int holdSzam { get; set; }
+        public double terfogat { get; set; }
+
+
+        public Solsys(string s)
+        {
+            
+            string[] v = s.Split(';');
+            bolygo = v[0];
+            holdSzam = int.Parse(v[1]);
+            terfogat = Convert.ToDouble(v[2]);
+            
+        
+        }
+
+    }
+}
